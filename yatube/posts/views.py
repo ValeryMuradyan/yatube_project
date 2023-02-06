c60ee8f62ab1b70a392627f4824e25541df68479
Valery Muradyan <v_muradyan@list.ru>
2023-02-06 14:40:04 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+# Главная страница
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request):
+    return HttpResponse('Посты')
