8c8c8031ce31e8aa64f3c5261c215aa3aeb2d5d9
Valery Muradyan <v_muradyan@list.ru>
2023-02-09 01:43:24 +0300

    name and namespace

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,9 +7,19 @@ from django.http import HttpResponse
 # Главная страница
 def index(request):
     template = 'posts\index.html'
-    return render(request, template)
+    title = 'YaTube'
+    context = {
+        'title': title,
+        'text': 'Это главная страница проекта Yatube'
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts\group_list.html'
+    title = 'YaTube'
+    context = {
+        'title': title,
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
     return render(request, template)
