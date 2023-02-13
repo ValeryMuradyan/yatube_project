2b6ecf1014571aeb515dced7a58cd1f70395148f
Valery Muradyan <v_muradyan@list.ru>
2023-02-13 12:00:09 +0300

    исправления перед уроком 6/6

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -5,7 +5,12 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('pk', 'text', 'pub_date', 'author') 
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author'
+    ) 
     # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',) 
     # Добавляем возможность фильтрации по дате
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 # Главная страница
 def index(request):
-    template = 'posts\index.html'
-    title = 'Главная страница Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': title, 
+        'posts': posts, 
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
